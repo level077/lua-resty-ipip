a063daf23eeedcc5598615a93a6e6ac95ba8f80f
level077 <level077@163.com>
2017-02-03 16:01:51 +0800

    add lua-resty-ipip

---
+++ README.md
@@ -0,0 +1,35 @@
+ipip.net付费版IP库的lua解析, ip库信息请查看http://ipip.net
+
+find.lua
+----------------
+输入一个ip，返回IP具体信息的json串。同时利用ngx.shared.dict做缓存，提高处理能力。
+
+
+ipdatX.lua
+----------------
+每日高级版lua解析代码。需根据ip库存放位置，修改相关代码。
+
+ipstation.lua
+----------------
+基站数据lua解析代码。需根据ip库存放位置，修改相关代码。
+
+Usage
+---------------
+请求方式为：curl '127.0.0.1/ip?ip=8.8.8.8'，nginx.conf相关配置如下：
+```
+http {
+   lua_package_path '/path/to/?.lua;;';
+   lua_shared_dict ip_cache 100m;
+   ......
+   server {
+       ......
+       location /ip {
+           content_by_lua_file "/ptah/to/find.lua";
+       }
+   }
+}
+```
+返回结果:
+```
+{"data":{"longitude":"","city":"","province":"GOOGLE","provider":"谷歌公司","continent":"*","company":"","country":"GOOGLE","tz2":"","cn_code":"","country_code":"*","tel_code":"","latitude":"","tz1":""},"code":0,"message":""}
+```
---
+++ find.lua
@@ -0,0 +1,33 @@
+local ipx = require "lua-resty-ipip.ipdatX"
+local ipstation = require "lua-resty-ipip.ipstation"
+local cjson = require "cjson"
+local args = ngx.req.get_uri_args()
+local ip = args["ip"]
+if not ip then
+	ngx.say("require ip")
+	ngx.exit(400)
+end
+
+local ip_cache = ngx.shared.ip_cache
+local value,err = ip_cache:get(ip)
+if value then
+        ngx.say(value)
+        ngx.exit(200)
+end
+local info=(ipx.split(ipx.IpLocationX(ip),"\t"))
+if not info then
+	local t = {code = 404, message = "not found", data = ""}
+	ngx.say(cjson.encode(t))
+	return ngx.exit(200)
+end
+local t = {code = 0, message = "", data = {country = info[1], province = info[2], city = info[3], company = info[4], provider = info[5],longitude = info[6], latitude = info[7],tz1 = info[8],tz2 = info[9], cn_code = info[10],tel_code = info[11],country_code = info[12],continent = info[13]}}
+local s = ipstation.IpLocationX(ip)
+if ipstation.IpLocationX(ip) then
+	t.data.station = 1
+end
+local value = cjson.encode(t)
+local succ,err,force = ip_cache:set(ip,value,43200)
+if not succ then
+        ngx.log(ERR,err)
+end
+ngx.say(value)
